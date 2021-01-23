2b24eb95239daf3d26054fe5941f0b19497869fb
Alec-Atlas <alec.atlas@kellogg.northwestern.edu>
2021-01-22 21:51:57 -0600

    complete lab

--- lab/week3.js
+++ lab/week3.js
@@ -25,6 +25,42 @@ let promotion3Icon          // expects a String (emoji)
 // - 🤑 for discount
 // - 🎰 for contest
 
+console.log(database)
+numberOfProducts = database.products.length
+products = database.products
+
+let promo1 = database.promotions[getRandomInt(database.promotions.length - 1)]
+let promo2 = database.promotions[getRandomInt(database.promotions.length - 1)]
+let promo3 = database.promotions[getRandomInt(database.promotions.length - 1)]
+
+promotion1Description = promo1.description
+promotion2Description = promo2.description
+promotion3Description = promo3.description
+
+if (promo1.type == 'discount') {
+  promotion1Icon = '🤑'
+} else if (promo1.type == 'shipping') {
+  promotion1Icon = '🚛'
+} else {
+  promotion1Icon = '🎰'
+}
+
+if (promo2.type == 'discount') {
+  promotion2Icon = '🤑'
+} else if (promo2.type == 'shipping') {
+  promotion2Icon = '🚛'
+} else {
+  promotion2Icon = '🎰'
+}
+
+if (promo3.type == 'discount') {
+  promotion3Icon = '🤑'
+} else if (promo3.type == 'shipping') {
+  promotion3Icon = '🚛'
+} else {
+  promotion3Icon = '🎰'
+}
+
 // HINTS
 // - Use Array.length to determine the total number of products/promotions in the database
 // - To generate a random number between 0 and x in Javascript, use our
